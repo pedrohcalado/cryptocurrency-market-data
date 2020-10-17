a8bb72aebe85013f04b76b8d94194b6e6341b6c3
pedrohcalado <pedrocalado22@gmail.com>
2020-10-17 19:48:04 -0300

    Add a date filter on the candle stick chart

--- cryptocurrency-data/src/components/CandleStickChart.js
+++ cryptocurrency-data/src/components/CandleStickChart.js
@@ -6,15 +6,41 @@ import { Chart } from "react-google-charts";
 export default function CandleStickChart(props) {
   const { currencyPair } = props;
   const [chartData, setChartData] = useState([]);
+  const [month, setMonth] = useState([1577887988, 1580479988]);
+  // const [month, setMonth] = useState([moment().startOf('month').unix(), moment().valueOf()]);
+
   useEffect(() => {
-    fetchChartData(currencyPair).then(data => setChartData(data));
-  }, [currencyPair]);
+    fetchChartData(currencyPair, month).then(data => setChartData(data));
+  }, [currencyPair, month]);
+
   const chartDataArr = chartData.map(t => [moment.unix(t.date).format('MM/DD/YYYY'), t.low, t.open, t.close, t.high]);
   chartDataArr.unshift(['day', 'low', 'open', 'close', 'high']);
+  const chartMonth = () => {
+    return (
+      <div>
+        <p>Select a month in 2020:</p>
+        <select onChange={(e) => setMonth(e.target.value.split(","))}>
+          <option value={[1577887988, 1580479988]}>January</option>
+          <option value={[1580566388, 1582985588]}>February</option>
+          <option value={[1583071988, 1585663988]}>March</option>
+          <option value={[1585750388, 1588255988]}>April</option>
+          <option value={[1588342388, 1590934388]}>May</option>
+          <option value={[1591020788, 1593526388]}>June</option>
+          <option value={[1593612788, 1596204788]}>July</option>
+          <option value={[1596291188, 1598796788]}>August</option>
+          <option value={[1598969588, 1601475188]}>September</option>
+          <option value={[1601561588, 1604153588]}>October</option>
+          <option value={[1604239988, 1606745588]}>November</option>
+          <option value={[1606831988, 1609423988]}>December</option>
+        </select>
+      </div>
+    );
+  }
 
   return (
     <div>
       <h3>Candlestick chart</h3>
+      {chartMonth()}
       <Chart
         width={'100%'}
         height={350}
--- cryptocurrency-data/src/services/fetchChartData.js
+++ cryptocurrency-data/src/services/fetchChartData.js
@@ -1,5 +1,5 @@
-export default function fetchChartData(currencyPair) {
-  const URL = `https://poloniex.com/public?command=returnChartData&currencyPair=${currencyPair}&start=1580566388&end=1583158388&period=14400`;
+export default function fetchChartData(currencyPair, [start, end]) {
+  const URL = `https://poloniex.com/public?command=returnChartData&currencyPair=${currencyPair}&start=${start}&end=${end}&period=14400`;
 
   return fetch(URL)
     .then((response) => response.json())
