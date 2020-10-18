04d766a77ccb6ffd6caff33886d5f8a9c5c3c70b
pedrohcalado <pedrocalado22@gmail.com>
2020-10-18 18:11:24 -0300

    Include loading text for the order book and trade history components

--- cryptocurrency-data/src/components/OrderBook.js
+++ cryptocurrency-data/src/components/OrderBook.js
@@ -4,48 +4,58 @@ import './OrderBook.css';
 
 export default function OrderBook(props) {
   const { currencyPair } = props;
-  const [orderBook, setOrderBook] = useState()
+  const [orderBook, setOrderBook] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
-    fetchOrderBook(currencyPair).then((order) => setOrderBook(order));
+    setIsLoading(true);
+    fetchOrderBook(currencyPair).then((order) => {
+      setOrderBook(order);
+      setIsLoading(false);
+    });
   }, [currencyPair]);
 
   return (
     <div>
       <h3 className="ob-title">Order book</h3>
-      <div className="table--ob-container">
-        <table className="table-striped table-bordered table-sm">
-          <thead>
-            <tr>
-              <td>Asks</td>
-              <td>Quantity</td>
-            </tr>
-          </thead>
-          <tbody>
-            {orderBook && orderBook.asks.map(order =>
-              <tr key={order[0]}>
-                <td>{order[0]}</td>
-                <td>{order[1]}</td>
+      {isLoading && <p>Loading...</p>}
+      {!isLoading &&
+        <div className="table--ob-container">
+          <table className="table-striped table-bordered table-sm">
+            <thead>
+              <tr>
+                <td>Asks</td>
+                <td>Quantity</td>
               </tr>
-            )}
-          </tbody>
-        </table>
-        <table className="table-striped table-bordered table-sm">
-          <thead>
-            <tr>
-              <td>Bid</td>
-              <td>Quantity</td>
-            </tr>
-          </thead>
-          <tbody>
-            {orderBook && orderBook.bids.map(order =>
-              <tr key={order[0]}>
-                <td>{order[0]}</td>
-                <td>{order[1]}</td>
+            </thead>
+            <tbody>
+              {orderBook && orderBook.asks.map(order =>
+                <tr key={order[0]}>
+                  <td>{order[0]}</td>
+                  <td>{order[1]}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+          <table className="table-striped table-bordered table-sm">
+            <thead>
+              <tr>
+                <td>Bids</td>
+                <td>Quantity</td>
               </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {orderBook && orderBook.bids.map(order =>
+                <tr key={order[0]}>
+                  <td>{order[0]}</td>
+                  <td>{order[1]}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      }
+    {(orderBook && orderBook.length === 0) && <p>No order found</p>}
     </div>
   );
 }
--- cryptocurrency-data/src/components/TradeHistory.js
+++ cryptocurrency-data/src/components/TradeHistory.js
@@ -5,42 +5,53 @@ import './TradeHistory.css';
 export default function TradeHistory(props) {
   const { currencyPair } = props;
   const [tradeHistory, setTradeHistory] = useState();
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
-    fetchTradeHistory(currencyPair).then((trades) => setTradeHistory(trades))
-  }, [currencyPair])
+    setIsLoading(true);
+    fetchTradeHistory(currencyPair).then((trades) => {
+      setTradeHistory(trades);
+      setIsLoading(false);
+    })
+  }, [currencyPair]);
+
   return (
     <div>
       <h3 className="th-title">Trade history</h3>
-      <div className="th-table-container">
-        <table className="table-striped table-bordered table-sm">
-          <thead>
-            <tr>
-              <td>Global trade ID</td>
-              <td>Trade ID</td>
-              <td>Date</td>
-              <td>Type</td>
-              <td>Rate</td>
-              <td>Amount</td>
-              <td>Total</td>
-              <td>Order number</td>
-            </tr>
-          </thead>
-          <tbody>
-            {tradeHistory && tradeHistory.map(trade =>
-              <tr key={trade.tradeID}>
-                <td>{trade.globalTradeID}</td>
-                <td>{trade.tradeID}</td>
-                <td>{trade.date}</td>
-                <td>{trade.type}</td>
-                <td>{trade.rate}</td>
-                <td>{trade.amount}</td>
-                <td>{trade.total}</td>
-                <td>{trade.orderNumber}</td>
+      <p>Last 200 trades</p>
+      {isLoading && <p>Loading...</p>}
+      {!isLoading &&
+        <div className="th-table-container">
+          <table className="table-striped table-bordered table-sm">
+            <thead>
+              <tr>
+                <td>Global trade ID</td>
+                <td>Trade ID</td>
+                <td>Date</td>
+                <td>Type</td>
+                <td>Rate</td>
+                <td>Amount</td>
+                <td>Total</td>
+                <td>Order number</td>
               </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {tradeHistory && tradeHistory.map(trade =>
+                <tr key={trade.tradeID}>
+                  <td>{trade.globalTradeID}</td>
+                  <td>{trade.tradeID}</td>
+                  <td>{trade.date}</td>
+                  <td>{trade.type}</td>
+                  <td>{trade.rate}</td>
+                  <td>{trade.amount}</td>
+                  <td>{trade.total}</td>
+                  <td>{trade.orderNumber}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      }
+    {(tradeHistory && tradeHistory.length === 0) && <p>No trade found</p>}
     </div>
   );
 }
